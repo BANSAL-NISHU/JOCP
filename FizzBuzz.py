beee80de9a7285d1268658f3a4d34cf30ba8da96
NISHU BANSAL <bansalnishu418@gmail.com>
2021-02-15 16:55:17 +0530

    Code for FizzBuzz

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -2,18 +2,19 @@
 Any number divisible by three is replaced by the word Fizz.
 Any number divisible by five is replaced by the word Buzz.
 Numbers divisible by 3 and 5 both (or 15) become FizzBuzz.
+
 """
 
 r = int(input("Enter the maximum range: "))
-for i in range(1,r):
-    if(i%3==0 and i%5==0):
+
+for i in range(1, r):
+    if i % 3 == 0 and i % 5 == 0:
         print("FizzBuzz")
     else:
-        if(i%3==0):
-            print("Fizz")
+        if i % 5 == 0:
+            print("Buzz")
         else:
-            if(i%5==0):
-                print("Buzz")
+            if i % 3 == 0:
+                print("Fizz")
             else:
                 print(i)
-            
