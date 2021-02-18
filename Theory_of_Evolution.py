c96b50e33d46e33bcd0e3bfc41018003c52a666a
NISHU BANSAL <bansalnishu418@gmail.com>
2021-02-18 10:45:28 +0530

    Code for Theory of Evolution

--- Theory_of_Evolution.py
+++ Theory_of_Evolution.py
@@ -6,16 +6,18 @@ def evolve(x):
     p = random.randint(1, 100)
     if p == 1:
         if x[index] == '0':
-            x[index] == '1'
+            x[index] = '1'
         else:
-            x[index] == '0'
+            x[index] = '0'
 
 
 with open("dna_data.txt") as myfile:
     x = myfile.read()
     x = list(x)
+    print(len(x))
 
-for i in range(len(x)):
+
+for i in range(0, 10000):
     evolve(x)
 
 print(x)
