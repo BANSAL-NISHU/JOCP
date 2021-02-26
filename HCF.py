6b550a55bb7b07cc82c215f62f245413f0051ee1
NISHU BANSAL <bansalnishu418@gmail.com>
2021-02-26 18:25:17 +0530

    Program to find HCF of two numbers.

--- HCF.py
+++ HCF.py
@@ -25,7 +25,7 @@ Applications:
 num1 = int(input())
 num2 = int(input())
 i = 1
-while i <= num1 and i <= num2:           # loop stops runs till the smallest number between num1 and num2
+while i <= num1 and i <= num2:           # loop runs till the smallest number between num1 and num2
     if num1 % i == 0 and num2 % i == 0:
         output = i                        # hcf = i
     i = i + 1
