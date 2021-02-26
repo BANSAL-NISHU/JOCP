6b504cc2814291c3b797abef70820d283fa68a2e
NISHU BANSAL <bansalnishu418@gmail.com>
2021-02-26 18:24:52 +0530

    Program to find HCF of two numbers.

--- HCF.py
+++ HCF.py
@@ -10,13 +10,23 @@ Applications:
 
 """
 
-m = int(input("Enter first number: "))
-n = int(input("Enter second number: "))
+# m = int(input("Enter first number: "))
+# n = int(input("Enter second number: "))
+#
+# x = min(m, n)  # smallest number between two given numbers
+#
+# for i in range(1, x + 1):
+#     if m % i == 0 and n % i == 0:
+#         hcf = i
+#
+# print("HCF of " + str(m) + " and " + str(n) + " is " + str(hcf) + ".")
 
-x = min(m,n)     #smallest number between two given numbers
 
-for i in range(1,x+1):
-    if m%i==0 and n%i==0:
-        hcf = i
-
-print("HCF of " + str(m) + " and " + str(n) + " is " + str(hcf) + ".")
+num1 = int(input())
+num2 = int(input())
+i = 1
+while i <= num1 and i <= num2:           # loop stops runs till the smallest number between num1 and num2
+    if num1 % i == 0 and num2 % i == 0:
+        output = i                        # hcf = i
+    i = i + 1
+print(output)
